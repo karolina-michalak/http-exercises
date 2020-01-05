38c7fadb80929bb63243890bb5b0b8ef136baae7
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-05 20:11:05 +0100

    request intreceptors

--- src/index.js
+++ src/index.js
@@ -13,5 +13,14 @@ axios.interceptors.request.use(request => {
     return Promise.reject(error)
 })
 
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response
+}, error => {
+    console.log(error)
+    return Promise.reject(error)
+})
+
+
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
